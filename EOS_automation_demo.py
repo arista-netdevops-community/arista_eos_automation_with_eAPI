349446ffaea82387e20c0065d2b66410f968495e
khelil sator <ksator@arista.com>
2020-05-15 02:35:38 +0200

    m

--- EOS_automation_demo.py
+++ EOS_automation_demo.py
@@ -2,28 +2,33 @@ import yaml
 from jinja2 import Template
 import os
 from jsonrpclib import Server
+import time 
 
 username = "arista"
 password = "arista"
 
+# Template to generate devices configuration 
 f=open('config.j2')
 s=f.read()
 f.close()
 eos_template=Template(s)
 
+# Directory to save devices configuration 
 cwd = os.getcwd()
 config_directory = os.path.dirname(cwd + "/config/")
 if not os.path.exists(config_directory):
     os.makedirs(config_directory)
 
+# Devices variables 
 inventory_f=open('inventory.yml', 'r')
 inventory_s=inventory_f.read()
 inventory_f.close()
 inventory=yaml.load(inventory_s, Loader=yaml.FullLoader)
 
+# Genrating the devices configuration
 for device in inventory:
     print ('-'*60)
-    print ('rendering the template for device ' + device)
+    print ('Generating the template for device ' + device)
     f=open(inventory[device]['vars'])
     s=f.read()
     f.close()
@@ -32,6 +37,7 @@ for device in inventory:
     conf.write(eos_template.render(vars))
     conf.close()
 
+# configuring the devices
 for device in inventory:
     print ('-'*60)
     print ('configuring the device ' + device)
@@ -42,4 +48,20 @@ for device in inventory:
     conf_list = f.read().splitlines()
     f.close() 
     conf = switch.runCmds(version=1,cmds=conf_list, autoComplete=True)
+
+# auditing the devices
+print ('-'*60)
+print ('audit will start in 15 seconds')
+time.sleep(15)
+
+for device in inventory:
+    print ('-'*60)
+    print ('Auditing the device ' + device)
+    ip = inventory[device]['ip']
+    url = "http://" + username + ":" + password + "@" + ip + "/command-api"
+    switch = Server(url)
+    result=switch.runCmds(version=1,cmds=["sh ver"], format='json', autoComplete=True)
+    print(result[0]['modelName'])
+
+
 
