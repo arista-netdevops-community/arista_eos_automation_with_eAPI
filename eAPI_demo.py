c8f5295b3dee2f83466539b5a90a568ae4387a06
khelil sator <ksator@arista.com>
2020-05-15 13:45:45 +0200

    m

--- eAPI_demo.py
+++ eAPI_demo.py
@@ -64,13 +64,18 @@ for device in inventory:
 
 # auditing the devices
 print ('-'*60)
-print ('audit will start in 15 seconds')
-time.sleep(15)
+print ('audit will start in 15 seconds ...')
+time.sleep(5)
+print ('audit will start in 10 seconds ...')
+time.sleep(5)
+print ('audit will start in 5 seconds ...')
+time.sleep(5)
 
-# auditing all BGP neigbors configured on all the devices (i.e we use the devices configuration as a SoT) 
+# auditing all BGP neighbors configured on all the devices (i.e we use the devices configuration as a SoT) 
 for device in inventory:
     print ('-'*60)
     print ('Auditing all BGP neighbors configured on the device ' + device)
+    print ('i.e we are currently using the device configuration as a SoT') 
     ip = inventory[device]['ip']
     url = "http://" + username + ":" + password + "@" + ip + "/command-api"
     switch = Server(url)
@@ -80,6 +85,37 @@ for device in inventory:
         print ("the number of IPv4 prefixes sent to the BGP neighbor " + item['peerAddress'] + " is " + str(item['prefixesSent']))
         print ("the number of IPv4 prefixes received from the BGP neighbor " + item['peerAddress'] + " is " + str(item['prefixesReceived']))
 
+# auditing only the desired BGP neigbhors on all the devices (i.e we use the devices variables as a SoT) 
+for device in inventory:
+    print ('-'*60)
+    print ('Auditing only the desired BGP neighbors on the device ' + device)
+    print ('i.e we are currently using the device variables as a SoT') 
+    # Generating required show commands' 
+    f=open(inventory[device]['vars'])
+    s=f.read()
+    f.close()
+    vars = yaml.load(s, Loader=yaml.FullLoader)
+    f=open('templates/bgp_audit.j2')
+    s=f.read()
+    f.close()
+    bgp_audit_template=Template(s)
+    audit = open("audit/" + device +  "_bgp_audit.txt",'w')
+    audit.write(bgp_audit_template.render(vars))
+    audit.close()
+    # Running required show commands 
+    audit = open("audit/" + device +  "_bgp_audit.txt",'r')
+    show_commands_list = audit.read().splitlines()
+    ip = inventory[device]['ip']
+    url = "http://" + username + ":" + password + "@" + ip + "/command-api"
+    switch = Server(url)
+    for item in show_commands_list: 
+        if(item == " "):
+            show_commands_list.remove(" ")
+    result = switch.runCmds(version=1,cmds=show_commands_list)
+    for item in result: 
+        print ("the BGP session with " + item['vrfs']['default']['peerList'][0]['peerAddress'] + " is " + item['vrfs']['default']['peerList'][0]['state'])
+        print ("the number of IPv4 prefixes sent to the BGP neighbor " + item['vrfs']['default']['peerList'][0]['peerAddress'] + " is " + str(item['vrfs']['default']['peerList'][0]['prefixesSent']))
+        print ("the number of IPv4 prefixes received from the BGP neighbor " + item['vrfs']['default']['peerList'][0]['peerAddress'] + " is " + str(item['vrfs']['default']['peerList'][0]['prefixesReceived']))
 
 
 
